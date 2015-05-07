853e62ad45465f6270365e56fccfa5ac8bd1b60e
iris-chang <aiching1997@gmail.com>
2015-05-07 11:05:06 -0400

    just updates on the new classes

--- Traps.java
+++ Traps.java
@@ -12,7 +12,8 @@ public class Traps {
 	public static final int SPIKES = 0;
 	public static final int CIRC_SAW = 1;
 	public static final int NINJA_STAR = 2;
-	public static final int LASER = 3;	
+	public static final int LASER = 3;
+	public static final int SWING_SAW =4;
     public Traps() {
     }
     
