434ff4808682bcbc87c15df7e48c42e702825163
iris-chang <aiching1997@gmail.com>
2015-05-11 11:05:41 -0400

    Stuff I did to the new classes
    Still under construction. I need to iron out the details soon.

--- Traps.java
+++ Traps.java
@@ -14,8 +14,17 @@ public class Traps {
 	public static final int NINJA_STAR = 2;
 	public static final int LASER = 3;
 	public static final int SWING_SAW =4;
+	public static final int ROT_STAR = 5;
+	private boolean stationary;
+	private int x,y;
+	private int dx,dy;
+	private int speed;
     public Traps() {
+    	
     }
     
-    
+    public move(){
+    //should the main program check if the trap is stationary before calling this method or implement the distinction withint the method?
+    	
+    }
 }
